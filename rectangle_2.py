9e39bce5d9a81950be22a60150956cd9c5083f2b
Pavel_Tsvetkov <76658162+Pavel253@users.noreply.github.com>
2021-03-01 17:15:54 +0300

    Задание 6.10.1
    Создайте класс любых геометрических фигур (например, прямоугольник), где на выход получаем характеристики фигуры. 
    
    Каждый экземпляр должен иметь атрибуты x, y, width и height (зависит от выбранной фигуры).
    
    Вы должны иметь возможность передавать атрибуты при создании, например, для прямоугольника, следующим образом (где x = 5, y = 10, width = 50, height = 100 в этом порядке).
    
    Создайте метод, который возвращает прямоугольник как строку (подсказка: реализация str). Для объекта прямоугольника со значениями атрибута x = 5, y = 10, width = 50, height = 100, метод должен вернуть строку Rectangle (5, 10, 50, 100).

---
+++ rectangle_2.py
@@ -0,0 +1,13 @@
+from rectangle import Rectangle, Square
+
+rect_1 = Rectangle(1, 5)
+rect_2 = Rectangle(5, 2)
+rect_3 = Rectangle(5, 10)
+square_1 = Square(10)
+
+
+print(str(rect_1.get_area()),
+    (rect_2.get_area()),
+    (rect_3.get_area()),
+    (square_1.get_area_square()))
+
